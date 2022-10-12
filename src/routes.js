1ad8682f0897ad3b3e7e492a3b3bc6031734f610
jaovw <joojvw@gmail.com>
2022-10-11 22:13:03 -0300

    rota

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express"
+import { logger } from "./utils/logger.js"
+import { insertDb } from "./db/insertDb.js"
 
 const router = Router()
+
+router.get('/', insertDb)
 
 export default router
