4dfc9f4511ba4afd2af962b1627ddda82c9a265a
jaovw <joojvw@gmail.com>
2022-10-13 18:16:29 -0300

    nova rota de cadastro

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { logger } from "./utils/logger.js"
 import { getListagem, getQueryListagem, getIdListagem } from "./controllers/listagemController.js"
+import cadastroController from "./controllers/cadastroController.js"
 
 const router = Router()
 
@@ -9,5 +10,7 @@ router.get('/universities', getListagem)
 router.get('/universities/:id', getIdListagem)
 
 router.get('/universities/country/:country?',getQueryListagem)
+
+router.post('/universities', cadastroController)
 
 export default router
