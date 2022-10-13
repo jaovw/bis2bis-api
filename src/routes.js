2a08962b1a4e97f321e2ed01abb6bc8f4faab6dd
jaovw <joojvw@gmail.com>
2022-10-13 16:29:02 -0300

    novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express"
 import { logger } from "./utils/logger.js"
-import { getListagem, getQueryListagem } from "./controllers/listagemController.js"
+import { getListagem, getQueryListagem, getIdListagem } from "./controllers/listagemController.js"
 
 const router = Router()
 
 router.get('/universities', getListagem)
-router.get('/universities/:country?', getQueryListagem)
+
+router.get('/universities/:id', getIdListagem)
+
+router.get('/universities/country/:country?',getQueryListagem)
 
 export default router
