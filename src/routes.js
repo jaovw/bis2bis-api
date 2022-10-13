d293601f91cb260a01cfd4643d1c9e69945f6371
jaovw <joojvw@gmail.com>
2022-10-13 14:54:49 -0300

    rotas api

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import { logger } from "./utils/logger.js"
-// import { insertDb } from "./db/insertDb.js"
+import { getListagem, getQueryListagem } from "./controllers/listagemController.js"
 
 const router = Router()
 
-// router.get('/', insertDb)
+router.get('/universities', getListagem)
+router.get('/universities/:country?', getQueryListagem)
 
 export default router
