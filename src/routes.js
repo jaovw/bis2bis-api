2eb14f6c9b4bf3a8fe8cb366c1e49a42545e4347
jaovw <joojvw@gmail.com>
2022-10-13 18:59:14 -0300

    nova rota api

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { logger } from "./utils/logger.js"
 import { getListagem, getQueryListagem, getIdListagem } from "./controllers/listagemController.js"
 import cadastroController from "./controllers/cadastroController.js"
+import atualizacaoController from "./controllers/atualizacaoController.js"
 
 const router = Router()
 
@@ -12,5 +13,7 @@ router.get('/universities/:id', getIdListagem)
 router.get('/universities/country/:country?',getQueryListagem)
 
 router.post('/universities', cadastroController)
+
+router.patch('/universities/:id', atualizacaoController)
 
 export default router
