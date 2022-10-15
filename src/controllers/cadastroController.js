9ba2ca3b274bbd6f2a2eec8c0b77cc4e590fbdf3
jaovw <joojvw@gmail.com>
2022-10-15 00:18:10 -0300

    validacoes no cadastro

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -4,19 +4,34 @@ import model from "../models/universidade.js"
 import stringUp from "../utils/stringUp.js"
 
 
-export default async function(req, res) {
+export default async function (req, res) {
 
-    const {
+    let {
         name,
+        country,
+        alpha_two_code,
         web_pages,
         domains,
         state_province
     } = req.body
-    
-    //  PADRONIZANDO CAMPOS 
-    const country = stringUp(req.body.country)
-    const alpha_two_code = req.body.alpha_two_code.toUpperCase()
 
+    //  VALIDACOES
+    if (country) {
+        stringUp(country)
+    } else {
+        return res.status(500).json({ message: `Utilize o body corretamente.` })
+    }
+
+    if (alpha_two_code) {
+        alpha_two_code.toUpperCase()
+    } else {
+        return res.status(500).json({ message: `Utilize o body corretamente.` })
+    }
+
+    if (!name || !country || !alpha_two_code) {
+        res.set('Content-Type', 'text/html')
+        return res.status(500).json({ message: `Utilize o body corretamente.` })
+    }
     const universidade = {
         country,
         alpha_two_code,
@@ -27,16 +42,29 @@ export default async function(req, res) {
     }
 
     try {
-        
+
         await dbConnect()
+
+        const cad = await model.findOne({
+            name: universidade.name,
+            country: universidade.country,
+            alpha_two_code: universidade.alpha_two_code
+        })
+
+        if (cad) {
+            return res.status(500).json({ message: `Item ja existe.` })
+        }
+        
         await model.create(universidade)
 
         return res.status(201).json({
             obj: name,
             message: 'Item cadastrado com sucesso !'
         })
+
+
     } catch (e) {
-       logger.error(e)
-       res.status(500).json({error: e}) 
+        logger.error(e)
+        res.status(500).json({ error: e })
     }
 } 
