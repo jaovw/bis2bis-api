3932efc827f3cb556aa1abe586e42e85b53f1add
jaovw <joojvw@gmail.com>
2022-10-14 23:23:46 -0300

    orientacoes de acesso

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const app = express()
 app.use(express.json())
 app.use(router)
 
-const server = app.listen(port, () => logger.info('Rodando ...'))
+const server = app.listen(port, () => logger.info(
+    `
+        API rodando na porta ${port}.
+        Caso queira acessar o ambiente AWS >> http://ec2-18-213-245-167.compute-1.amazonaws.com:3000/universities
+        Caso queira acessar o ambiente local ou Docker >> http://localhost:3000/universities
+    `
+))
 
 export { app, server }
